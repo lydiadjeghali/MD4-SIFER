9c343a81ef9a2734b146cbfb5de77f09af4f3bec
lydiadjeghali <120333289+lydiadjeghali@users.noreply.github.com>
2025-12-02 16:09:54 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def cesar_cipher(message, key): 
+
+        crypted_message = ""
+        for char in massage :
+            crypted_char = char((ord(char)+key) % 1_114_112)
+            crypted_message += crypted_message
+
+
+        return crypted_message
+
